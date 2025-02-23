85ee2e9a4790efbf0e64f2dc31021bf4918cd7c0
kbr <mkbirundha2407@gmail.com>
2025-02-23 12:32:34 +0530

    Merge pull request #4 from BirundhaProjects/dev-nagu
    Connected to DB & Secured PORT & Connection String

--- React-Todo/server/server/server.js
+++ React-Todo/server/server/server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
+const mongodb = require("mongodb").MongoClient
+const {PORT, MONGODB_URL} = require('../config/index');
 
 app.use(cors()); // Enable CORS
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
+
+//Connect to MongoDB
+let database;
+let connectdb = async () => {
+  const connection = await mongodb.connect(MONGODB_URL)
+  database = await connection.db("todo_Database");
+  console.log("Connected to MongoDB...!👍");
+  await database.createCollection("todo");
+  console.log("Created todo collection....!");
+}
+connectdb()
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
